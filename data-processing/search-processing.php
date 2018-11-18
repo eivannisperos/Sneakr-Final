4f3343df9c47eff66b317ba727239cea9e719495
Eivan <enispero@sfu.ca>
2018-11-17 21:41:49 -0800

    ordered search start
    - created function to order incoming data from database by date

--- data-processing/search-processing.php
+++ data-processing/search-processing.php
@@ -16,6 +16,7 @@
       $result = mysqli_query($connection, $query);
 
       while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        $dateValue = assignMonthValue($row['releaseMonth']);
         // $dataName = $dataIndex;
         // we type cast it because when json encodes it, any illegal characters will make json_encode($data) = false
         $search_result = array(
@@ -25,11 +26,16 @@
           "releaseDay" => (int) $row['releaseDay'],
           "releaseMonth" => (string)$row['releaseMonth'],
           "releaseYear" => (int)$row['releaseYear'],
-          "imgLink" => (string)$row['imgLink']
+          "imgLink" => (string)$row['imgLink'],
+          "dateValue" => $dateValue
         );
 
         array_push($data, $search_result);
       }
+
+      $monthSort = array_column($search_result, 'dateValue');
+      $daySort = array_column($search_result, 'releaseDay');
+      //array_multisort($monthSort, SORT_ASC, $daySort, SORT_DESC, $search_result);
 
       if (empty($data)) {
         $data["dataEmpty"] = true;
@@ -41,9 +47,32 @@
   unset($_SESSION["search-input"]);
   echo json_encode($data);
 
-  // function assignMonthValue($month) {
-  //   switch($month) {
-  //     case "January"
-  //   }
-  // }
+  function assignMonthValue($month) {
+    switch($month) {
+      case "January":
+        return 1;
+      case "Febuary":
+        return 2;
+      case "March":
+        return 3;
+      case "April":
+        return 4;
+      case "May":
+        return 5;
+      case "June":
+        return 6;
+      case "July":
+        return 7;
+      case "August":
+        return 8;
+      case "September":
+        return 9;
+      case "October":
+        return 10;
+      case "November":
+        return 11;
+      case "December":
+        return 12;
+    }
+  }
 ?>
